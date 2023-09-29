d1c81eab15de7f8e5f5b8240d42a23de754b7ffc
Sergio-Cardenas-Gracia <126247317+Sergio-Cardenas-Gracia@users.noreply.github.com>
2023-09-29 14:01:00 +0200

    Use cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Data Science project that uses a cookiecutter template.',
+    author='Sergio Cárdenas, Armand de Asís & Jan Sallent',
+    license='MIT',
+)
