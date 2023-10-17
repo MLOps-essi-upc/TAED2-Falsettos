9e0f0ba4f84974efc7d47dd75511bde5c3538c07
armand-07 <armand.de.asis@estudiantat.upc.edu>
2023-10-17 22:03:41 +0200

    Added model predictions and data sampling

--- src/__init__.py
+++ src/__init__.py
@@ -6,8 +6,41 @@ load_dotenv()
 
 ROOT_DIR = Path(Path(__file__).resolve().parent.parent)
 
-RAW_DATA_DIR = ROOT_DIR / "data/raw"
-PROCESSED_DATA_DIR = ROOT_DIR / "data/processed"
+RAW_DATA_DIR = ROOT_DIR / "data" / "raw"
+PROCESSED_DATA_DIR = ROOT_DIR / "data" / "processed"
+RAW_DATA_SAMPLE = ROOT_DIR / "data" / "raw_sample_example"
 
 METRICS_DIR = ROOT_DIR / "metrics"
 MODELS_DIR = ROOT_DIR / "models"
+
+
+UNKNOWN_WORDS_V1 = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "bed",
+    "bird",
+    "cat",
+    "dog",
+    "happy",
+    "house",
+    "marvin",
+    "sheila",
+    "tree",
+    "wow",
+]
+
+UNKNOWN_WORDS_V2 = UNKNOWN_WORDS_V1 + [
+    "backward",
+    "forward",
+    "follow",
+    "learn",
+    "visual",
+]
