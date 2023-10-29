f60a588eba470d4cc2a3d513923fda2517832245
Sergio-Cardenas-Gracia <sergio.cardenas@estudiantat.upc.edu>
2023-10-29 13:05:27 +0100

    Apply pylint to the src/tests folder (and fix test_api.py)

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,28 +1,36 @@
-import pytest
+"""
+This module contains the api testing.
+"""
+
+import os
 from http import HTTPStatus
+
+import pytest
+import pandas as pd
 from fastapi.testclient import TestClient
-from src.app.api import app
 
 from src import ROOT_DIR
-import os
-import pandas as pd
+from src.app.api import app
 
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def client():
+    """Define client object for running pytest."""
     with TestClient(app) as client:
         return client
 
 
-# @pytest.fixture
-# def payload():
-#     RAW_DATA_SAMPLE = os.path.join(ROOT_DIR, 'data', 'raw_sample_example', 'sample_example.pkl')
-#     sample_df = pd.read_pickle(RAW_DATA_SAMPLE)
-#     array_string = str(sample_df["audio_array"][0].tobytes())
-#     return array_string
+@pytest.fixture
+def payload():
+    """Define payload object for running pytest."""
+    raw_data_sample = os.path.join(ROOT_DIR, 'data', 'raw_sample_example', 'sample_example.pkl')
+    sample_df = pd.read_pickle(raw_data_sample)
+    array_string = str(sample_df["audio_array"][0].tobytes())
+    return {'audio_array': array_string}
 
 
 def test_api_init(client):
+    """Test the api initialization."""
     response = client.get("/")
     json = response.json()
     assert response.status_code == 200
@@ -33,16 +41,14 @@ def test_api_init(client):
     assert json["status-code"] == 200
 
 
-# def test_api_model(client, payload):
-#     label = 20
-#     predicted_command = "bed"
-#     response = client.post("/predict", payload)
-#     assert response.status_code == 200
-#     assert response.json() == {
-#         "message": HTTPStatus.OK.phrase,
-#         "status-code": HTTPStatus.OK,
-#         "data": {
-#             "predicted_label": label,
-#             "predicted_command": predicted_command,
-#         },
-#     }
+def test_api_model(client, payload):
+    """Test the api model prediction."""
+    label = 20
+    predicted_command = "Bed"
+    response = client.post("/predict", json=payload)
+    json_response = response.json()
+    assert response.status_code == 200
+    assert json_response["message"] == HTTPStatus.OK.phrase
+    assert json_response["status-code"] == HTTPStatus.OK
+    assert json_response["data"]["predicted_label"] == label
+    assert json_response["data"]["predicted_command"] == predicted_command
